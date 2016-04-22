87f3ebe143d1e39e266837662746bf875981b94b
Sterv <stephensulliv@gmail.com>
2016-04-22 11:00:48 -0700

    adding env variable for port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 app.use(express.static(__dirname));
-var port = 8000;
+var port = process.env.PORT  || 8000;
 
 app.listen(port, function(){
   console.log("Listening on port",port);
