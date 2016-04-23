185c512561f4ad142f7d350b1cd2de5e7d97314a
Sterv <stephensulliv@gmail.com>
2016-04-22 17:14:35 -0700

    Refactor app.js to mobile

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var video_out = document.getElementById("vid-box");
 
 function login(form) {
+
   var phone = window.phone = PHONE({
-      number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+      number        : $('#username').val() || "Anonymous", // listen on username line else Anonymous
       publish_key   : 'pub-c-566d8d42-99d0-4d21-bc72-6d376ed70567',
       subscribe_key : 'sub-c-107b4e72-082d-11e6-996b-0619f8945a4f',
       ssl : (('https:' == document.location.protocol) ? true : false)
   }); 
-  phone.ready(function(){ form.username.style.background="#55ff5b"; });
+
+  phone.ready(function(){ 
+    //form.username.style.background="#55ff5b";
+  });
   phone.receive(function(session){
       session.connected(function(session) { video_out.appendChild(session.video); });
       session.ended(function(session) { video_out.innerHTML=''; });
   });
+
+
+
   return false;   // So the form does not submit.
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 // express --> instantiating a server
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 app.use(express.static(__dirname));
 var port = process.env.PORT  || 8000;
+
+app.use(function(req,res,next){
+  res.setHeader('Content-Type', 'text/html');
+  return next();
+})
+
+app.get('/', function(req,res){
+  fs.readFile('index.html', function(err, data){
+    console.log("get request to /")
+    res.send(data);
+  });
+})
+
+app.get('/peer', function(req,res){
+  fs.readFile('index-peer.html', function(err, data){
+    console.log("get request to /peer")
+    res.send(data);
+  });
+})
+
+app.get('/group', function(req,res){
+  fs.readFile('index-group.html', function(err, data){
+    console.log("get request to /group")
+    res.send(data);
+  });
+})
 
 app.listen(port, function(){
   console.log("Listening on port",port);
